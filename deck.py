984e1a7e365e0998a2355df83fb00b20bdad3df8
peawyoyoyin <peawyoyoyin@gmail.com>
2016-08-04 23:42:39 +0700

    card & hand library

---
+++ deck.py
@@ -0,0 +1,92 @@
+import random
+
+class Card(object):
+    """
+    basic Card object.
+
+    attributes:
+        name: the name of the card
+        isfacedown: boolean stating if the card is faced down; faced down cards' names are not shown in repr
+    """
+    def __init__(self,name,facedown=False):
+        """construct a card from a string name. defaults face-up"""
+        self.name = name
+        self.isfacedown = facedown
+
+    def __str__(self):
+        if(self.isfacedown):
+            return "card: face-down"
+        else:
+            return "card: " + self.name
+    
+    def __repr__(self):
+        if(self.isfacedown):
+            return "card: face-down"
+        else:
+            return "card: " + self.name
+
+    def tofaceup(self):
+        """forces the card into face-up state"""
+        self.isfacedown=False
+    def tofacedown(self):
+        """forces the card into face-down state"""
+        self.isfacedown=True
+    def flip(self):
+        self.isfacedown = not self.isfacedown
+
+class Deck(object):
+    """
+    Deck is a pile of cards with ordered arrangement.
+
+    attributes:
+        deck: list of cards in the deck with 0 is the index of the top card.
+    """
+    def __init__(self,cards):
+        """construct a Deck object with container containing Card objects as .deck"""
+        self.deck = list(cards)
+        
+    def cards_in_deck(self):
+        return len(self.deck)
+    
+    def add_card_totop(self,card):
+        """add a card to the top of the deck, equivalent to insert_card(0,card)"""
+        self.deck.insert(0,card)
+
+    def add_card_tobottom(self,card):
+        """add a card to the bottom of the deck"""
+        self.deck.append(card)
+
+    def add_cards_totop(self,cards):
+        """add a pile of cards to the top of the deck"""
+        self.deck = self.deck + list(cards)
+
+    def add_cards_tobottom(self,cards):
+        """add a pile of cards to the bottom of the deck"""
+        self.deck = list(cards) + self.deck
+        
+    def insert_card(self,index,card):
+        """insert a card to the deck, similar to list.insert"""
+        self.deck.insert(index,card)
+
+    def insert_card_random(self,card):
+        """insert a card into a random place in the deck"""
+        self.deck.insert(random.randint(0,len(self.deck)),card)
+        
+    def top_card(self):
+        """returns the top card of the deck"""
+        try:
+            return self.deck[0]
+        except:
+            raise ValueError("top_card error") 
+    
+    def draw_card(self):
+        """removes the top card from the deck and returns it"""
+        try:
+            card_drawn = self.deck.pop(0)
+            return card_drawn
+        except:
+            raise ValueError("draw_card error")
+        
+    def shuffle(self):
+        """shuffles the cards in the deck using random.shuffle"""
+        random.shuffle(self.deck)
---
+++ player_hand.py
@@ -0,0 +1,43 @@
+import random
+
+class Player():
+    def __init__(self,name):
+        self.name = name
+        self.hand = Hand()
+
+class Hand():
+    """
+    Hand is a list of cards in a player's hand
+    
+    attributes:
+        cards: list of cards in hand
+    """
+    
+    def __init__(self,cards):
+        """construct a Hand object from cards"""
+        self.cards = list(cards)
+    def number_of_cards(self):
+        """returns total number of cards in hand"""
+        return len(self.cards)
+    def add_card(self,card):
+        """add a card to hand"""
+        self.cards.append(card)
+    def shuffle(self):
+        """shuffle cards in hand"""
+        random.shuffle(self.cards)
+    def discard_index(self,index):
+        """remove the card of the index from hand.cards and returns it"""
+        discarding = self.pop(index)
+        return discarding
+    def discard(self,card):
+        """remove the specified card of the hand and returns it, return None if there are no cards of specified type in the hand"""
+        if card in self.cards:
+            ret = card
+            self.cards.remove(card)
+            return ret
+        else:
+            return None
+    def discard_random(self):
+        """remove a random card from the hand and returns it"""
+        discarding = self.pop(random.randint(0,len(cards)))
+        return discarding
